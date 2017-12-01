a8b0950c9a8f6e31b436426231c12669e6d6a135
王博 <wangboforyou@163.com>
2017-12-01 11:13:32 +0800

    初始化项目

---
+++ happyLottery/happyLottery/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  happyLottery
+//
+//  Created by 王博 on 2017/12/1.
+//  Copyright © 2017年 onlytechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
