1f78db885d969bb442fa1a282cd6aae73c306245
王博 <wangboforyou@163.com>
2017-12-01 13:07:44 +0800

    初始化项目

---
+++ happyLottery/happyLottery/Class/Base/Controller/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  happyLottery
+//
+//  Created by 王博 on 2017/12/1.
+//  Copyright © 2017年 onlytechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
