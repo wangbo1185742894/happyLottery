14330e1d589f7e7fa7f8bff1401119677fece020
LYJ <278762817@qq.com>
2018-01-10 18:58:39 +0800

    待定文件

---
+++ happyLottery/happyLottery/Class/LotteryMine(我的)/Controller/MyHelpViewController.m
@@ -0,0 +1,71 @@
+//
+//  MyHelpViewController.m
+//  happyLottery
+//
+//  Created by LYJ on 2017/12/12.
+//  Copyright © 2017年 onlytechnology. All rights reserved.
+//  帮助
+
+
+#import "MyHelpViewController.h"
+#import "CouponGuidViewController.h"
+#import "GetIntegralViewController.h"
+
+@interface MyHelpViewController ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *top;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottom;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollerview;
+
+@end
+
+@implementation MyHelpViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"帮助";
+    if ([self isIphoneX]) {
+        self.top.constant = 88;
+        self.bottom.constant = 34;
+    }
+}
+
+- (IBAction)footballPlay:(id)sender {
+}
+
+- (IBAction)getIntegral:(id)sender {
+  
+    GetIntegralViewController * mpVC = [[GetIntegralViewController alloc]init];
+    [self.navigationController pushViewController:mpVC animated:YES];
+}
+- (IBAction)integralChange:(id)sender {
+}
+//功能指南
+- (IBAction)FunctionalGuide:(id)sender {
+}
+
+//优惠券指南
+
+- (IBAction)CouponGuide:(id)sender {
+    CouponGuidViewController * mpVC = [[CouponGuidViewController alloc]init];
+    [self.navigationController pushViewController:mpVC animated:YES];
+}
+
+- (IBAction)question:(id)sender {
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ happyLottery/happyLottery/Class/LotteryMine(我的)/Controller/ShareViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShareViewController.h
+//  happyLottery
+//
+//  Created by LYJ on 2018/1/3.
+//  Copyright © 2018年 onlytechnology. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ShareViewController : BaseViewController
+
+@end
