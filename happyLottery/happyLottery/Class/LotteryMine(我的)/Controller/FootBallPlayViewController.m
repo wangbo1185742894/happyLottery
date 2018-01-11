96620526f47ffcfcf5afcb8aeaa3315ff51e0524
LYJ <278762817@qq.com>
2018-01-11 14:17:41 +0800

    竞彩足球玩法

--- happyLottery/happyLottery/Class/LotteryMine(我的)/Controller/FootBallPlayViewController.m
+++ happyLottery/happyLottery/Class/LotteryMine(我的)/Controller/FootBallPlayViewController.m
@@ -7,8 +7,24 @@
 //
 
 #import "FootBallPlayViewController.h"
+#import "LotteryProfileSelectView.h"
 
-@interface FootBallPlayViewController ()
+@interface FootBallPlayViewController ()<LotteryProfileSelectViewDelegate>{
+    UIButton * titleBtn;
+     LotteryProfileSelectView *profileSelectView;
+}
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UIView *selectView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *top;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottom;
+
+@property (weak, nonatomic) IBOutlet UIButton *spfBtn;
+
+@property (weak, nonatomic) IBOutlet UIButton *bfBtn;
+@property (weak, nonatomic) IBOutlet UIButton *rqspfBtn;
+@property (weak, nonatomic) IBOutlet UIButton *jqsBtn;
+@property (weak, nonatomic) IBOutlet UIButton *bqcBtn;
+@property (weak, nonatomic) IBOutlet UIButton *hhggNtm;
 
 @end
 
@@ -16,7 +32,95 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title = @"竞足玩法介绍";
+   // self.title = @"竞足玩法介绍";
+    [self setTitleView];
+ [self loadHtml:@"jingcaizuqiu_shengpingfu"];
+}
+
+-(void)loadHtml:(NSString*)htmlname{
+    
+    NSString *path = [[NSBundle mainBundle] bundlePath];
+    NSURL *baseURL = [NSURL fileURLWithPath:path];
+    NSString * htmlPath = [[NSBundle mainBundle] pathForResource:htmlname
+                                                          ofType:@"html"];
+    NSString * htmlCont = [NSString stringWithContentsOfFile:htmlPath
+                                                    encoding:NSUTF8StringEncoding
+                                                       error:nil];
+    [self.webView loadHTMLString:htmlCont baseURL:baseURL];
+    
+   
+}
+
+-(void)setTitleView{
+  
+    
+   
+    titleBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    titleBtn.frame = CGRectMake(0, 10, 150, 40);
+    [titleBtn addTarget:self action:@selector(showProfileType) forControlEvents:UIControlEventTouchUpInside];
+    [titleBtn setTitle:@"竞足玩法介绍" forState:0];
+    [titleBtn setImage:[UIImage imageNamed:@""] forState:0];
+    titleBtn.titleLabel.font = [UIFont systemFontOfSize:18];
+//    if (profileSelectView == nil) {
+//  profileSelectView = [[LotteryProfileSelectView alloc]initWithFrame:CGRectMake(0, 64, KscreenWidth, KscreenHeight - 64)];
+//    }
+//    profileSelectView.delegate = self;
+//    //_selectView.lotteryPros = self.profiles;
+//    profileSelectView.hidden = YES;
+//    [self.view addSubview:profileSelectView];
+    self.navigationItem.titleView = titleBtn;
+    
+}
+
+-(void)showProfileType{
+    _selectView.hidden = !_selectView.hidden;
+    if (_selectView.hidden==YES) {
+          self.bfBtn.selected=NO;
+        self.spfBtn.selected=NO;
+        self.rqspfBtn.selected=NO;
+         self.jqsBtn.selected=NO;
+        self.bqcBtn.selected=NO;
+         self.hhggNtm.selected=NO;
+    }
+}
+
+- (IBAction)BFClick:(id)sender {
+     [titleBtn setTitle:@"比分" forState:0];
+    self.bfBtn.selected=YES;
+     [self loadHtml:@"jingcaizuqiu_bifen"];
+        [self showProfileType];
+}
+
+- (IBAction)SPFClick:(id)sender {
+      [titleBtn setTitle:@"胜平负" forState:0];
+    self.spfBtn.selected=YES;
+  
+    [self loadHtml:@"jingcaizuqiu_shengpingfu"];
+      [self showProfileType];
+}
+- (IBAction)RQSPFClick:(id)sender {
+      [titleBtn setTitle:@"让球胜平负" forState:0];
+    self.rqspfBtn.selected=YES;
+    [self loadHtml:@"jingcaizuqiu_rangqiushengpingfu"];
+     [self showProfileType];
+}
+- (IBAction)JQSClick:(id)sender {
+      [titleBtn setTitle:@"进球数" forState:0];
+    self.jqsBtn.selected=YES;
+    [self loadHtml:@"jingcaizuqiu_zongjinqiushu"];
+     [self showProfileType];
+}
+- (IBAction)BQCClick:(id)sender {
+      [titleBtn setTitle:@"半全场" forState:0];
+    self.bqcBtn.selected=YES;
+     [self loadHtml:@"jingcaizuqiu_banquanchang"];
+     [self showProfileType];
+}
+- (IBAction)HHGGClick:(id)sender {
+      [titleBtn setTitle:@"混合过关" forState:0];
+    self.hhggNtm.selected=YES;
+    [self loadHtml:@"jingcaizuqiu_hunheguoguan"];
+     [self showProfileType];
 }
 
 - (void)didReceiveMemoryWarning {
