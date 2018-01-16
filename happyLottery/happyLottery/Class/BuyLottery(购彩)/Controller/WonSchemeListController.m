c0034313f8b900b06859725c10767f865dc3e520
王博 <wangboforyou@163.com>
2018-01-16 10:58:55 +0800

    修改bug 赛事收藏   预测 界面

---
+++ happyLottery/happyLottery/Class/BuyLottery(购彩)/Controller/WonSchemeListController.m
@@ -0,0 +1,29 @@
+//
+//  WonSchemeListController.m
+//  happyLottery
+//
+//  Created by 王博 on 2018/1/15.
+//  Copyright © 2018年 onlytechnology. All rights reserved.
+//
+
+#import "WonSchemeListController.h"
+
+@interface WonSchemeListController ()
+
+@end
+
+@implementation WonSchemeListController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+
+
+@end
