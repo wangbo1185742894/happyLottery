8651c575972f95803fb53177a0452b73c20a180d
LYJ <278762817@qq.com>
2018-01-30 10:05:37 +0800

    线上地址

--- happyLottery/happyLottery/Utility/API.h
+++ happyLottery/happyLottery/Utility/API.h
@@ -10,13 +10,13 @@
 #define API_h
 
 
-//#define WSServerURL @"http://118.190.43.29:28086/services%@"
-//#define ServerAddress @"http://192.168.88.244:8086"  //资讯  轮播图 用户图像
-//#define H5BaseAddress @"http://118.190.43.29:28000"
-
+#define WSServerURL @"http://118.190.43.29:28000/services%@"
 #define ServerAddress @"http://192.168.88.244:8086"  //资讯  轮播图 用户图像
+#define H5BaseAddress @"http://118.190.43.29:28086"
+
+//#define ServerAddress @"http://192.168.88.244:8086"  //资讯  轮播图 用户图像
 //#define H5BaseAddress @"http://192.168.88.244:18086"
-#define H5BaseAddress @"http://192.168.88.193:18086"  //谢青服务
+//#define H5BaseAddress @"http://192.168.88.193:18086"  //谢青服务
 //#define H5BaseAddress @"http://192.168.88.116:18086"  //史少鹏服务
 
 //杨芳本地
@@ -27,7 +27,7 @@
 
 
 //244服务
-#define WSServerURL @"http://192.168.88.244:28000/services%@"
+//#define WSServerURL @"http://192.168.88.244:28000/services%@"
 
 //史少鹏服务
 //#define WSServerURL @"http://192.168.88.116:28000/services%@"
