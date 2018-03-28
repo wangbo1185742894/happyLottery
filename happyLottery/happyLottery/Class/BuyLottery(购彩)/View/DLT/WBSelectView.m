961c60022931471ce072616f9e9d2749ee60a9ce
LYJ <278762817@qq.com>
2018-03-28 16:52:40 +0800

    大乐透数据条数ui修改

--- happyLottery/happyLottery/Class/BuyLottery(购彩)/View/DLT/WBSelectView.m
+++ happyLottery/happyLottery/Class/BuyLottery(购彩)/View/DLT/WBSelectView.m
@@ -41,61 +41,19 @@
     UITableViewCell*cell = [tableView dequeueReusableCellWithIdentifier:@"dataCell"];
     if (cell ==nil) {
         cell  = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"dataCell"];
-        
+        //label居中
+        UILabel *lable = [[UILabel alloc]init];
+        lable.text = self.dataArray[indexPath.row];
+        lable.textColor = TEXTGRAYCOLOR;
+        [lable sizeToFit];
+        lable.frame = CGRectMake((self.frame.size.width-lable.frame.size.width)/2, (cell.frame.size.height-lable.frame.size.height)/2, lable.frame.size.width, lable.frame.size.height);
+        [cell addSubview:lable];
     }
-    cell.textLabel.text = self.dataArray[indexPath.row];
-    cell.textLabel.textColor = TEXTGRAYCOLOR;
-    
-    {
-        cell.selectedBackgroundView = [[UIView alloc] initWithFrame:cell.bounds];
-        cell.selectedBackgroundView.backgroundColor = CellSelectedColor;
-        tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-        UIView *view;
-        if ([cell subviews].count>1) {
-            view = [cell subviews][1];
-        }
-        
-        CGRect downLineFrame = view.frame;
-        [view removeFromSuperview];
-        if ([self respondsToSelector:@selector(tableView:heightForRowAtIndexPath:)]) {
-            downLineFrame.origin.y = [self tableView:tableView heightForRowAtIndexPath:indexPath] - SEPHEIGHT;
-        }else{
-            downLineFrame.origin.y = 44 - SEPHEIGHT;
-        }
-        
-        downLineFrame.origin.x = SEPLEADING;
-        downLineFrame.size.width = tableView.frame.size.width - 2*SEPLEADING;
-        downLineFrame.size.height = SEPHEIGHT;
-        UILabel *downLine = [[UILabel alloc] init];
-        downLine.backgroundColor = SEPCOLOR;
-        NSInteger lineCount = [self tableView:tableView numberOfRowsInSection:indexPath.section];
-        if (indexPath.row == lineCount - 1) {
-            downLineFrame.origin.x = 0;
-            downLineFrame.size.width = tableView.frame.size.width;
-            //            downLineFrame.origin.y -= 1;
-            //            downLine.frame =
-        }//else{
-        //downLine.frame = downLineFrame;
-        //        }
-        downLine.frame = downLineFrame;
-        [cell addSubview:downLine];
-        
-        if (indexPath.row == self.index) {
-            cell.accessoryType = UITableViewCellAccessoryCheckmark;
-        }else{
-            cell.accessoryType = UITableViewCellAccessoryNone;
-            
-        }
-        
-        if (indexPath.row == 0) {
-            UILabel *upLine = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, KscreenWidth, SEPHEIGHT)];
-            upLine.backgroundColor = SEPCOLOR;
-            [cell addSubview:upLine];
-            
-        }
-
+    if (indexPath.row == self.index) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }else{
+        cell.accessoryType = UITableViewCellAccessoryNone;
     }
-    
     cell.selectedBackgroundView = [[UIView alloc] initWithFrame:cell.bounds];
     cell.selectedBackgroundView.backgroundColor = CellSelectedColor;
     return cell;
