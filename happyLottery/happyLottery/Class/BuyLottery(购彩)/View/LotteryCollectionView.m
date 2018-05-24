04c9aa7edf877b379e9343b8f43833cb4b97ec65
LYJ <278762817@qq.com>
2018-05-24 09:08:16 +0800

    collection 首页初稿

---
+++ happyLottery/happyLottery/Class/BuyLottery(购彩)/View/LotteryCollectionView.m
@@ -0,0 +1,21 @@
+//
+//  LotteryCollectionView.m
+//  happyLottery
+//
+//  Created by LYJ on 2018/5/23.
+//  Copyright © 2018年 onlytechnology. All rights reserved.
+//
+
+#import "LotteryCollectionView.h"
+
+@implementation LotteryCollectionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
