04c6bf635d88799f6263f3891a7f06ba6f12914b
王博 <wangbo3919@163.com>
2018-06-01 11:50:07 +0800

    屏蔽as

--- happyLottery/happyLottery/Utility/API.h
+++ happyLottery/happyLottery/Utility/API.h
@@ -8,7 +8,7 @@
 #ifndef API_h
 #define API_h
 
-#define APPSTORE 1 //appstore 版本 打开宏，  内测包 ，自由平台  屏蔽宏
+//#define APPSTORE 1 //appstore 版本 打开宏，  内测包 ，自由平台  屏蔽宏
 
 #define NameSpaceURI @"http://webservice.onlytest.com/"
 
