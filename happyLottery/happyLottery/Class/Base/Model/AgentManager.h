7abb803f5328a7336b463bf52750069481539716
LYJ <278762817@qq.com>
2018-06-02 08:59:36 +0800

    API添加

--- happyLottery/happyLottery/Class/Base/Model/AgentManager.h
+++ happyLottery/happyLottery/Class/Base/Model/AgentManager.h
@@ -9,12 +9,184 @@
 #import "Manager.h"
 
 @protocol AgentManagerDelegate
+
 @optional
+
+
+/**
+ 会员建圈申请
+ */
 -(void )agentApplydelegate:(NSDictionary *)param isSuccess:(BOOL)success errorMsg:(NSString *)msg;
+
+
+/**
+获取当前用户的圈子信息
+ */
+-(void )getAgentInfodelegate:(NSDictionary *)param isSuccess:(BOOL)success errorMsg:(NSString *)msg;
+
+
+
+/**
+ 获取当前用户所在圈子的所有圈民列表
+ */
+-(void )listAgentMemberdelegate:(NSArray *)array isSuccess:(BOOL)success errorMsg:(NSString *)msg;
+
+
+/**
+ 获取当前登录用户的圈子可跟单方案数
+ */
+-(void )getAgentFollowCountdelegate:(NSString *)string isSuccess:(BOOL)success errorMsg:(NSString *)msg;
+
+
+/**
+ 获取当前登录用户的圈子可跟单方案
+ */
+-(void )listAgentFollowdelegate:(NSArray *)array isSuccess:(BOOL)success errorMsg:(NSString *)msg;
+
+
+/**
+ 获取圈子的动态
+ */
+-(void )listAgentDynamicdelegate:(NSArray *)array isSuccess:(BOOL)success errorMsg:(NSString *)msg;
+
+
+/**
+ 获取当前用户的圈子信息
+ */
+-(void )getMyAgentInfodelegate:(NSDictionary *)param isSuccess:(BOOL)success errorMsg:(NSString *)msg;
+
+
+/**
+ 佣金账户转个人账户
+ */
+-(void )transferAccountdelegate:(NSString *)string isSuccess:(BOOL)success errorMsg:(NSString *)msg;
+
+
+/**
+ 分页获取圈主的佣金转账记录
+ */
+-(void )listMyTransferdelegate:(NSArray *)array isSuccess:(BOOL)success errorMsg:(NSString *)msg;
+
+
+/**
+变更圈子的头像
+ */
+-(void )modifyHeadUrldelegate:(NSString *)string isSuccess:(BOOL)success errorMsg:(NSString *)msg;
+
+
+/**
+变更圈名
+
+ */
+-(void )modifyCircleNamedelegate:(NSString *)string isSuccess:(BOOL)success errorMsg:(NSString *)msg;
+
+
+/**
+ 申请变更圈子公告
+ */
+-(void )modifyNoticedelegate:(NSString *)string isSuccess:(BOOL)success errorMsg:(NSString *)msg;
+
 
 @end
 
 @interface AgentManager : Manager
 @property(weak,nonatomic)id<AgentManagerDelegate> delegate;
+
+/**
+ 会员建圈申请
+
+ @param param {"cardCode":"卡号","realName":"真实姓名","mobile":"手机号","qq":"qq号"}
+ */
 -(void )agentApply:(NSDictionary *)param;
+
+/**
+ 获取当前用户的圈子信息
+
+ @param param {"cardCode":"当前登录会员卡号"}
+ */
+- (void)getAgentInfo:(NSDictionary *)param;
+
+
+/**
+ 获取当前用户所在圈子的所有圈民列表(我的圈子->我的圈友)
+
+ @param param {"cardCode":"当前登录用户","page":"页码","pageSize":"分页"}
+ */
+- (void)listAgentMember:(NSDictionary *)param;
+
+
+/**
+ 获取当前登录用户的圈子可跟单方案数
+
+ @param param {"agentId":"当前圈子ID"}
+ */
+- (void)getAgentFollowCount:(NSDictionary *)param;
+
+
+/**
+ 获取当前登录用户的圈子可跟单方案
+
+ @param param {"agentId":"当前圈子ID","page":"xxx","pageSize":"xxx" }
+ */
+- (void)listAgentFollow:(NSDictionary *)param;
+
+
+
+/**
+ 获取圈子的动态
+
+ @param param {"agentId":"当前圈子ID"}
+ */
+- (void)listAgentDynamic:(NSDictionary *)param;
+
+
+/**
+ 获取我的圈子
+
+ @param param {"cardCode":"圈主卡号"}
+ */
+- (void)getMyAgentInfo:(NSDictionary *)param;
+
+
+
+/**
+ 佣金账户转个人账户
+
+ @param param {"agentId":"圈子ID","cardCode":"执行转账的卡号","transferCost":"转账金额"}
+ */
+- (void)transferAccount:(NSDictionary *)param;
+
+
+
+/**
+ 分页获取圈主的佣金转账记录
+
+ @param param  {"agentId":"圈子ID","page":"页码","pageSize":"分页记录数" }
+ */
+- (void)listMyTransfer:(NSDictionary *)param;
+
+
+/**
+ 变更圈子的头像
+
+ @param param {"agentId":"圈子ID","headUrl":"头像" }
+ */
+- (void)modifyHeadUrl:(NSDictionary *)param;
+
+
+/**
+ 变更圈名
+
+ @param param {"agentId":"圈子ID","circleName":"新的圈名" }
+ */
+- (void)modifyCircleName:(NSDictionary *)param;
+
+
+/**
+ 申请变更圈子公告
+
+ @param param {"agentId":"圈子ID","notice":"公告" }
+ */
+- (void)modifyNotice:(NSDictionary *)param;
+
 @end
--- happyLottery/happyLottery/Class/Base/Model/AgentManager.m
+++ happyLottery/happyLottery/Class/Base/Model/AgentManager.m
@@ -1,4 +1,4 @@
-//
+ //
 //  AgentManager.m
 //  happyLottery
 //
@@ -60,7 +60,7 @@ typedef  void(^RequestBlock)(AFHTTPRequestOperation *operation, id responseObjec
 }
 
 /**
- 会员建圈申请 {"cardCode":"xxx","realName":"xxx","mobile":"xxx","qq":"xxx"} cardCode 卡号 realName 真实姓名 mobile 手机号 qq qq号
+ 会员建圈申请
  */
 -(void )agentApply:(NSDictionary *)param{
     [self AgentManagerRequest:param url:Agent_agentApply andSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
@@ -72,6 +72,153 @@ typedef  void(^RequestBlock)(AFHTTPRequestOperation *operation, id responseObjec
     }];
 }
 
+/**
+ 获取当前用户的圈子信息
+ */
+- (void)getAgentInfo:(NSDictionary *)param{
+    
+    [self AgentManagerRequest:param url:Agent_getAgentInfo andSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate getAgentInfodelegate:[self analyticalDictionary:response] isSuccess:response.succeed errorMsg:response.errorMsg];
+    } andfailure:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate getAgentInfodelegate:nil isSuccess:NO errorMsg:response.errorMsg];
+    }];
+}
 
+
+/**
+ 获取当前用户所在圈子的所有圈民列表(我的圈子->我的圈友)
+ */
+- (void)listAgentMember:(NSDictionary *)param{
+    [self AgentManagerRequest:param url:Agent_listAgentMember andSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate listAgentMemberdelegate:[self analyticalArray:response] isSuccess:response.succeed errorMsg:response.errorMsg];
+    } andfailure:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate listAgentMemberdelegate:nil isSuccess:NO errorMsg:response.errorMsg];
+    }];
+}
+
+/**
+ 获取当前登录用户的圈子可跟单方案数
+ */
+- (void)getAgentFollowCount:(NSDictionary *)param{
+    [self AgentManagerRequest:param url:Agent_getAgentFollowCount andSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate getAgentFollowCountdelegate:[self analyticalString:response] isSuccess:response.succeed errorMsg:response.errorMsg];
+    } andfailure:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate getAgentFollowCountdelegate:nil isSuccess:NO errorMsg:response.errorMsg];
+    }];
+}
+
+/**
+ 获取当前登录用户的圈子可跟单方案
+ */
+- (void)listAgentFollow:(NSDictionary *)param{
+    [self AgentManagerRequest:param url:Agent_listAgentFollow andSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate listAgentFollowdelegate:[self analyticalArray:response] isSuccess:response.succeed errorMsg:response.errorMsg];
+    } andfailure:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate listAgentFollowdelegate:nil isSuccess:NO errorMsg:response.errorMsg];
+    }];
+}
+
+
+/**
+ 获取圈子的动态
+ */
+- (void)listAgentDynamic:(NSDictionary *)param{
+    [self AgentManagerRequest:param url:Agent_listAgentDynamic andSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate listAgentDynamicdelegate:[self analyticalArray:response] isSuccess:response.succeed errorMsg:response.errorMsg];
+    } andfailure:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate listAgentDynamicdelegate:nil isSuccess:NO errorMsg:response.errorMsg];
+    }];
+}
+
+/**
+ 获取我的圈子
+ */
+- (void)getMyAgentInfo:(NSDictionary *)param{
+    [self AgentManagerRequest:param url:Agent_getMyAgentInfo andSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate getMyAgentInfodelegate:[self analyticalDictionary:response] isSuccess:response.succeed errorMsg:response.errorMsg];
+    } andfailure:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate getMyAgentInfodelegate:nil isSuccess:NO errorMsg:response.errorMsg];
+    }];
+}
+
+
+/**
+佣金账户转个人账户
+ */
+- (void)transferAccount:(NSDictionary *)param{
+    [self AgentManagerRequest:param url:Agent_transferAccount andSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate transferAccountdelegate:[self analyticalString:response] isSuccess:response.succeed errorMsg:response.errorMsg];
+    } andfailure:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate transferAccountdelegate:nil isSuccess:NO errorMsg:response.errorMsg];
+    }];
+}
+
+
+/**
+ 分页获取圈主的佣金转账记录
+ */
+- (void)listMyTransfer:(NSDictionary *)param{
+    [self AgentManagerRequest:param url:Agent_listMyTransfer andSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate listMyTransferdelegate:[self analyticalArray:response] isSuccess:response.succeed errorMsg:response.errorMsg];
+    } andfailure:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate listMyTransferdelegate:nil isSuccess:NO errorMsg:response.errorMsg];
+    }];
+}
+
+
+/**
+ 变更圈子的头像
+ */
+- (void)modifyHeadUrl:(NSDictionary *)param{
+    [self AgentManagerRequest:param url:Agent_modifyHeadUrl andSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate modifyHeadUrldelegate:[self analyticalString:response] isSuccess:response.succeed errorMsg:response.errorMsg];
+    } andfailure:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate modifyHeadUrldelegate:nil isSuccess:NO errorMsg:response.errorMsg];
+    }];
+}
+
+/**
+变更圈名
+ */
+- (void)modifyCircleName:(NSDictionary *)param{
+    [self AgentManagerRequest:param url:Agent_modifyCircleName andSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate modifyCircleNamedelegate:[self analyticalString:response] isSuccess:response.succeed errorMsg:response.errorMsg];
+    } andfailure:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate modifyCircleNamedelegate:nil isSuccess:NO errorMsg:response.errorMsg];
+    }];
+}
+
+/**
+ 申请变更圈子公告
+ */
+- (void)modifyNotice:(NSDictionary *)param{
+    [self AgentManagerRequest:param url:Agent_modifyNotice andSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate modifyNoticedelegate:[self analyticalString:response] isSuccess:response.succeed errorMsg:response.errorMsg];
+    } andfailure:^(AFHTTPRequestOperation *operation, id responseObject) {
+        SOAPResponse *response = [self wrapSOAPResponse:operation.responseString];
+        [self.delegate modifyNoticedelegate:nil isSuccess:NO errorMsg:response.errorMsg];
+    }];
+}
 
 @end
