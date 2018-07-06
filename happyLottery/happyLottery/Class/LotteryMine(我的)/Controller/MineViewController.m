79af1cba831a8f2b90547b17ff4944a8443046f0
LYJ <278762817@qq.com>
2018-07-06 15:09:04 +0800

    bug 修改

--- happyLottery/happyLottery/Class/LotteryMine(我的)/Controller/MineViewController.m
+++ happyLottery/happyLottery/Class/LotteryMine(我的)/Controller/MineViewController.m
@@ -90,10 +90,10 @@
     self.viewControllerNo = @"A201";
     if ([self isIphoneX]) {
         self.topDIs.constant = -44;
-        self.topHeadCons.constant = 215;
+        self.topHeadCons.constant = 235;
     }else{
         self.topDIs.constant = -20;
-        self.topHeadCons.constant = 191;
+        self.topHeadCons.constant = 211;
     }
     self.viewChongZhi.layer.masksToBounds = YES;
     self.viewChongZhi.layer.cornerRadius = 12;
@@ -102,6 +102,8 @@
     } else {
         self.heightCon.constant = 448;
     }
+    self.noticeRedPointLab.layer.cornerRadius = 3;
+    self.noticeRedPointLab.layer.masksToBounds = YES;
 }
 
 
