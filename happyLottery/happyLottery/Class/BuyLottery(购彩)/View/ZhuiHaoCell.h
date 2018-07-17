27a5132389df88ef4e1cbbb3eb438acdc0f27a05
LYJ <278762817@qq.com>
2018-07-17 16:19:13 +0800

    分享添加动态内容

---
+++ happyLottery/happyLottery/Class/BuyLottery(购彩)/View/ZhuiHaoCell.h
@@ -0,0 +1,13 @@
+//
+//  ZhuiHaoCell.h
+//  happyLottery
+//
+//  Created by LYJ on 2018/7/17.
+//  Copyright © 2018年 onlytechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZhuiHaoCell : UITableViewCell
+
+@end
