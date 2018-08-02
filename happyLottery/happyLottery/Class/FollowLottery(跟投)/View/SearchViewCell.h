7078fea0c9919b26ed720199515c2a13bbb19232
LYJ <278762817@qq.com>
2018-08-02 17:09:30 +0800

    修改

---
+++ happyLottery/happyLottery/Class/FollowLottery(跟投)/View/SearchViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewCell.h
+//  happyLottery
+//
+//  Created by LYJ on 2018/8/2.
+//  Copyright © 2018年 onlytechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewCell : UITableViewCell
+
+@end
