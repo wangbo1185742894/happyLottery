a041f03f8dcfd127adace0b33a762d5e3e999149
LYJ <278762817@qq.com>
2018-11-19 12:51:54 +0800

    银联替换包

--- happyLottery/happyLottery/Libs/paymentcontrol/inc/UPPaymentControl.h
+++ happyLottery/happyLottery/Libs/paymentcontrol/inc/UPPaymentControl.h
@@ -4,7 +4,7 @@
 //
 //  Created by qcao on 15/10/20.
 //  Copyright © 2015年 China Unionpay Co.,Ltd. All rights reserved.
-//
+//  v3.3.11
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
